bb514af3aa05224b1f63a02966bb4ad3cf7ed38a
Everton Paiva <evertonpaiva@gmail.com>
2019-07-15 07:11:23 -0300

    Trabalhando com relacionamentos utilizando coleções

--- gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -1,7 +1,5 @@
 package br.com.alura;
 
-import java.util.List;
-
 public class TestaCurso {
 
 	public static void main(String[] args) {
@@ -12,9 +10,9 @@ public class TestaCurso {
 				
 		//javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com Coleções", 22));
 		
-		System.out.println(javaColecoes.getAulas());
-		
-		//Aula 03 - item 01 - 16:36
+		System.out.println(javaColecoes.getAulas());		
 	}
 }
